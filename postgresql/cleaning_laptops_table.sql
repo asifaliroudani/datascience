bd85fb0beb9c98ec920a9d6d425b46b51d6899be
asifaliroudani <asifaliroudani@hotmail.com>
2020-10-07 15:55:04 +0500

    creating tables and using joins

--- postgresql/cleaning_laptops_table.sql
+++ postgresql/cleaning_laptops_table.sql
@@ -94,3 +94,130 @@ SELECT DISTINCT initcap(cpu) as cpu,
 				POSITION(SPLIT_PART(cpu, ' ', 3) IN CPU)
 FROM laptops
 ORDER BY cpu
+
+SELECT DISTINCT initcap(cpu) as cpu, 
+				left(cpu,position(' ' IN cpu)-1) as cpu_maker, 
+				length(cpu) as length, 
+				length(cpu) - length(replace(cpu,' ','')) as total_space_occurence,
+				POSITION(SPLIT_PART(cpu, ' ', 3) IN CPU),
+				SPLIT_PART(cpu, ' ', 1),
+				SPLIT_PART(cpu, ' ', 2),
+				SPLIT_PART(cpu, ' ', 3),
+				SPLIT_PART(cpu, ' ', 4),
+				substring(cpu, length(cpu) - length(replace(cpu,' ','')),length(cpu))
+				
+FROM laptops
+ORDER BY cpu
+
+SELECT max(length(manufacturer)) FROM laptops
+
+CREATE TABLE manufacturer(
+							manufacturer_id integer primary key GENERATED ALWAYS AS IDENTITY,
+							manufacturer_name varchar(12),
+							manufacturer_status boolean
+)
+
+INSERT INTO manufacturer(manufacturer_name,manufacturer_status) values('A','T')
+
+SELECT *
+FROM manufacturer
+
+truncate table manufacturer
+
+SELECT DISTINCT initcap(manufacturer) as manufacturer, 'T'::boolean as manufacturer_status
+FROM laptops
+
+SELECT *
+FROM laptops
+
+INSERT INTO manufacturer(manufacturer_name,manufacturer_status)
+SELECT DISTINCT initcap(manufacturer) as manufacturer, 'T'::boolean as manufacturer_status
+FROM laptops
+
+TRUNCATE TABLE manufacturer
+
+DROP TABLE model_name
+
+CREATE TABLE model_name(
+							model_name_id integer primary key GENERATED ALWAYS AS IDENTITY,
+							model_name varchar(60),
+							model_name_status boolean
+)
+
+SELECT max(length(model_name)) 
+FROM laptops
+
+INSERT INTO model_name(model_name, model_name_status)
+SELECT DISTINCT INITCAP(model_name) AS model_name, 'T'::boolean as model_name_status 
+FROM laptops
+
+SELECT * FROM model_name
+
+CREATE TABLE fact_laptops(
+	
+							fact_id integer primary key GENERATED ALWAYS AS IDENTITY ,
+							manufacturer_id integer,
+							model_name_id integer
+)	
+
+SELECT 		l.manufacturer as laptop_manufacturer, l.model_name as laptop_model_name, 
+			manufacturer.manufacturer_id, manufacturer_name, 
+			modelname.model_name_id, modelname.model_name
+
+FROM laptops as l
+INNER JOIN manufacturer manufacturer on manufacturer.manufacturer_name = l.manufacturer
+INNER JOIN model_name modelname on modelname.model_name = l.model_name
+
+
+SELECT 		manufacturer.manufacturer_id,
+			modelname.model_name_id
+
+FROM laptops as l
+INNER JOIN manufacturer manufacturer on manufacturer.manufacturer_name = l.manufacturer
+INNER JOIN model_name modelname on modelname.model_name = l.model_name
+
+INSERT INTO fact_laptops(manufacturer_id, model_name_id)
+SELECT 		manufacturer.manufacturer_id,
+			modelname.model_name_id
+
+FROM laptops as l
+INNER JOIN manufacturer manufacturer on manufacturer.manufacturer_name = l.manufacturer
+INNER JOIN model_name modelname on modelname.model_name = l.model_name
+
+SELECT *
+FROM fact_laptops
+
+SELECT 		l.manufacturer as laptop_manufacturer, l.model_name as laptop_model_name, 
+			manufacturer.manufacturer_id, manufacturer_name, 
+			modelname.model_name_id, modelname.model_name
+
+FROM laptops as l
+LEFT JOIN manufacturer manufacturer on manufacturer.manufacturer_name = l.manufacturer
+LEFT JOIN model_name modelname on modelname.model_name = l.model_name
+
+SELECT 		l.manufacturer as laptop_manufacturer, l.model_name as laptop_model_name, 
+			manufacturer.manufacturer_id, manufacturer_name, 
+			modelname.model_name_id, modelname.model_name
+
+FROM laptops as l
+INNER JOIN manufacturer manufacturer on manufacturer.manufacturer_name = initcap(l.manufacturer)
+INNER JOIN model_name modelname on modelname.model_name = initcap(l.model_name)
+
+DROP TABLE fact_laptops
+
+CREATE TABLE fact_laptops(
+	
+							fact_id integer primary key GENERATED ALWAYS AS IDENTITY ,
+							manufacturer_id integer,
+							model_name_id integer
+)	
+
+INSERT INTO fact_laptops(manufacturer_id, model_name_id)
+SELECT 		manufacturer.manufacturer_id,
+			modelname.model_name_id
+FROM laptops as l
+INNER JOIN manufacturer manufacturer on manufacturer.manufacturer_name = initcap(l.manufacturer)
+INNER JOIN model_name modelname on modelname.model_name = initcap(l.model_name)
+
+
+
